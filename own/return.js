62d5f9a8a0c0a40755b0474d650d168f189af6ef
Corelio <100584937+Coreli0@users.noreply.github.com>
2024-05-17 21:47:58 +0200

    Create return.js
    Signed-off-by: Corelio <100584937+Coreli0@users.noreply.github.com>

---
+++ own/return.js
@@ -0,0 +1,24 @@
+var button = document.createElement('button');
+
+var img = document.createElement('img');
+img.src = 'https://resources.midway.ovh/iconfinder/return-arrow.png';
+img.alt = 'Go to Midway.ovh';
+img.style.width = '40px';
+img.style.height = '40px';
+img.style.filter = 'invert(100%)'
+
+button.appendChild(img);
+
+button.style.position = 'fixed';
+button.style.top = '10px';
+button.style.right = '10px';
+button.style.backgroundColor = 'transparent';
+button.style.border = 'none';
+button.style.cursor = 'pointer';
+button.style.zIndex = '1900';
+
+button.addEventListener('click', function() {
+    window.location.href = 'https://midway.ovh';
+});
+
+document.body.appendChild(button);
